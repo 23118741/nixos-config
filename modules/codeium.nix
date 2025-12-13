ff35084ceb60a6016e57001619761675cf98e0fe
guus <beercoblub@gmail.com>
2025-12-13 16:07:18 +0100

    .

--- modules/codeium.nix
+++ modules/codeium.nix
@@ -7,12 +7,17 @@
     extraPackages = with pkgs; [
       gzip
       curl
-      coreutils # <--- Changed from 'uname' to 'coreutils'
+      coreutils
     ];
 
     extraPlugins = with pkgs.vimPlugins; {
-      codeium-nvim = {
-        package = codeium-nvim;
+
+      # CHANGE: The package name is now 'windsurf-nvim'
+      windsurf-nvim = {
+        package = windsurf-nvim;
+
+        # We still try to require "codeium" first.
+        # If this fails later, we will try "windsurf".
         setup = ''
           require("codeium").setup({
               enable_chat = true
