ad83cb123d431ce20d8505e8ff0482c2997a875c
guus <beercoblub@gmail.com>
2025-12-13 16:30:50 +0100

    .

--- modules/home-manager/nvim.nix
+++ modules/home-manager/nvim.nix
@@ -11,24 +11,28 @@ in
 {
   imports = [
     inputs.nvf.homeManagerModules.default
-
-    # Import BOTH modules now
     ../../modules/avante.nix
     ../../modules/codeium.nix
   ];
 
   programs.nvf = {
     enable = true;
-    settings = generalConfig;
-  };
-  autocomplete.nvim-cmp = {
-    enable = true;
-    mappings = {
-      confirm = "<CR>";
 
-      # FIX: Move navigation to Ctrl+n/p so Tab is free for Codeium
-      next = "<C-n>";
-      previous = "<C-p>";
-    };
+    # Merge general config with our manual overrides
+    settings = lib.mkMerge [
+      generalConfig
+
+      # Manual Override Block
+      {
+        vim.autocomplete.nvim-cmp = {
+          enable = true;
+          mappings = {
+            confirm = "<CR>";
+            next = "<C-n>";
+            previous = "<C-p>";
+          };
+        };
+      }
+    ];
   };
 }
