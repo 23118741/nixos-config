11b27a0536b5fef57ec6d7a144b672af10315120
guus <beercoblub@gmail.com>
2025-12-13 13:10:51 +0100

    avante 1.09

--- modules/avante.nix
+++ modules/avante.nix
@@ -6,9 +6,11 @@
     extraPackages = with pkgs; [ curl ];
 
     extraPlugins = with pkgs.vimPlugins; {
+
+      # --- Dependencies ---
+      # We remove the 'setup' here because we will manually load it below
       dressing-nvim = {
         package = dressing-nvim;
-        setup = "require('dressing').setup()";
       };
       nui-nvim = {
         package = nui-nvim;
@@ -17,19 +19,21 @@
         package = plenary-nvim;
       };
 
+      # --- Main Plugin ---
       avante-nvim = {
         package = avante-nvim;
         setup = ''
+          -- 1. Force load the UI dependency FIRST
+          require('dressing').setup()
+
+          -- 2. Now load Avante
           require('avante').setup({
-            -- Change provider to gemini
             provider = "gemini", 
             
             providers = {
               gemini = {
-                __inherited_from = "openai",
-                api_key_name = "GEMINI_API_KEY",
-                endpoint = "https://generativelanguage.googleapis.com/v1beta/openai/",
                 model = "gemini-1.5-flash",
+                temperature = 0,
                 max_tokens = 4096,
               },
             },
