03c4452eb9f9120eab0d62fb0b4c52c2925a8b38
guus <beercoblub@gmail.com>
2025-12-14 10:26:15 +0100

    cs weg voor nu

--- modules/home-manager/nvim.nix
+++ modules/home-manager/nvim.nix
@@ -24,73 +24,128 @@ in
 
       # Manual Override Block
       {
-        vim.autocomplete.nvim-cmp = {
-          enable = true;
-          mappings = {
-            confirm = "<CR>";
-            next = "<C-n>";
-            previous = "<C-p>";
-          };
-        };
-
-        # Enhanced diagnostic display for multiple languages
-        lsp = {
-          diagnostic = {
+              vim = {
+                autocomplete.nvim-cmp = {
             enable = true;
-            signs = true;
-            virtual_text = {
+                  mappings = {
+                    confirm = "<CR>";
+                    next = "<C-n>";
+                    previous = "<C-p>";
+                  };
+                };
+      
+                # Enhanced diagnostic display for multiple languages
+                lsp = {
+                  diagnostic = {
+                    enable = true;
+                    signs = true;
+                    virtual_text = {
+                      enable = true;
+                      prefix = " ";
+                      space_char = " ";
+                    };
+                    update_in_insert = false;
+                    severity = {
+                      error = "error";
+                      warning = "warning";
+                      information = "info";
+                      hint = "hint";
+                    };
+                  };
+      
+                  typescript = {
+                    enable = true;
+                    on_attach = ''
+                      require('lsp_signature').on_attach()
+                      require('lsp-colors').setup()
+                    '';
+                  };
+      
+                  html = {
+                    enable = true;
+                    on_attach = ''
+                      require('lsp_signature').on_attach()
+                      require('lsp-colors').setup()
+                    '';
+                  };
+      
+                  css = {
+                    enable = true;
+                    on_attach = ''
+                      require('lsp_signature').on_attach()
+                      require('lsp-colors').setup()
+                    '';
+                  };
+      
+                  json = {
+                    enable = true;
+                    on_attach = ''
+                      require('lsp_signature').on_attach()
+                      require('lsp-colors').setup()
+                    '';
+                  };
+                };
+              };
+      
+                # Enhanced diagnostic display for multiple languages
+                lsp = {
+                  diagnostic = {
               enable = true;
-              prefix = " ";
-              space_char = " ";
+                    signs = true;
+                    virtual_text = {
+                      enable = true;
+                      prefix = " ";
+                      space_char = " ";
+                    };
+                    update_in_insert = false;
+                    severity = {
+                      error = "error";
+                      warning = "warning";
+                      information = "info";
+                      hint = "hint";
+                    };
             };
-            update_in_insert = false;
-            severity = {
-              error = "error";
-              warning = "warning";
-              information = "info";
-              hint = "hint";
+      
+                  # Language-specific LSP configurations
+                  csharp = {
+                    enable = true;
+                    on_attach = ''
+                      require('lsp_signature').on_attach()
+                      require('lsp-colors').setup()
+                    '';
             };
-          };
 
-          # Language-specific LSP configurations
-          csharp = {
-            enable = true;
-            on_attach = ''
-              require('lsp_signature').on_attach()
-              require('lsp-colors').setup()
-            '';
-          };
+                  typescript = {
+                    enable = true;
+                    on_attach = ''
+                      require('lsp_signature').on_attach()
+                      require('lsp-colors').setup()
+                    '';
+                  };
 
-          typescript = {
-            enable = true;
-            on_attach = ''
-              require('lsp_signature').on_attach()
-              require('lsp-colors').setup()
-            '';
-          };
+                  html = {
+                    enable = true;
+                    on_attach = ''
+                      require('lsp_signature').on_attach()
+                      require('lsp-colors').setup()
+                    '';
+                  };
 
-          html = {
-            enable = true;
-            on_attach = ''
-              require('lsp_signature').on_attach()
-              require('lsp-colors').setup()
-            '';
-          };
+                  css = {
+                    enable = true;
+                    on_attach = ''
+                      require('lsp_signature').on_attach()
+                      require('lsp-colors').setup()
+                    '';
+                  };
 
-          css = {
-            enable = true;
-            on_attach = ''
-              require('lsp_signature').on_attach()
-              require('lsp-colors').setup()
-            '';
-          };
-
-          json = {
-            enable = true;
-            on_attach = ''
-              require('lsp_signature').on_attach()
-              require('lsp-colors').setup()
-            '';
+                  json = {
+                    enable = true;
+                    on_attach = ''
+                      require('lsp_signature').on_attach()
+                      require('lsp-colors').setup()
+                    '';
+                  };
           };
         };
       }
