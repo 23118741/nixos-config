5dceb80b43ccb8e8d73b20073d5b8118fe15a396
guus <beercoblub@gmail.com>
2025-12-15 15:59:56 +0100

    windsurf opnieuw ff geprobeerd nu weer werkende versie right?

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -123,7 +123,6 @@
   nixpkgs.config.allowUnfree = true;
 
   programs.steam.enable = true;
-  programs.windsurf.enable = true;
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
